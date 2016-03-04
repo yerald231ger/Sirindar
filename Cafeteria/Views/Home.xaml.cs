eabe04d8ba1233106632d87d568515d795cd8b47
yerald231ger <yerald231ger@gmail.com>
2016-03-04 08:55:01 -0600

    ok
    ok

--- Cafeteria/Views/Home.xaml.cs
+++ Cafeteria/Views/Home.xaml.cs
@@ -1,23 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ServiciosCafeteria.AppModels;
 using ServiciosCafeteria;
 using Cafeteria.Cloak;
 using CNSirindar.Models;
 using System.Configuration;
-using System.Windows.Media.Animation;
 using ServiciosCafeteria.Interfaces;
 
 namespace Cafeteria.Views
